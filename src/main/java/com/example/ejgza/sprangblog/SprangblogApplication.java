7fdcd48cf6f6bce99ea42b5ba4a4a2c374ad8254
Elvin <elvinjohngalarza@gmail.com>
2020-04-07 19:48:24 -0400

    setup project and mysql db, need to configure spring sec. and set up user registration

---
+++ src/main/java/com/example/ejgza/sprangblog/SprangblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejgza.sprangblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprangblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprangblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejgza/sprangblog/SprangblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejgza.sprangblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprangblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
